a4e688f3f605775b9f05bdd3ae8cec9734e324b2
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-31 14:54:15 -0400

    longest-common-subsequence medium

---
+++ dp/longest-common-subsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = [];
+
+  for (let i = 0; i <= text1.length; i++) {
+    dp[i] = [];
+    for (let j = 0; j <= text2.length; j++) {
+      if (i == 0 || j == 0) {
+        dp[i][j] = 0;
+      } else if (text1[i - 1] == text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  console.log(dp);
+  return dp[text1.length][text2.length];
+};
+
+console.log(longestCommonSubsequence('abcde', 'ace'));
